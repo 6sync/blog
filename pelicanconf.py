68b6a95e057108ed4aab53e32abcca4fcbc18ed4
Sam Collinson <sam@6sync.com>
2013-10-07 23:33:39 +1300

    fix up settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Mario Danic'
-SITENAME = u'6sync blog'
+SITENAME = u'6sync | Blog'
 SITEURL = ''
 
 PATH = 'content/'
@@ -18,14 +18,10 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
 
